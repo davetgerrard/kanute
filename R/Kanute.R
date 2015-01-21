af00179933885ae04a6126a8b1ace5e115fe5546
Dave Gerrard <davetgerrard@gmail.com>
2015-01-21 15:08:37 +0000

    added Kanute.R to hold package info

---
+++ R/Kanute.R
@@ -0,0 +1,29 @@
+#' Kanute: unit-style behaviour testing for softwares and software versions.
+#'
+#' \tabular{ll}{
+#' Package: \tab Kanute\cr
+#' Type: \tab Package\cr
+#' Version: \tab 0.1\cr
+#' Date: \tab 2015-01-31\cr
+#' Author: \tab Dave T. Gerrard \cr
+#' License: \tab GPL (>= 2)\cr
+#' LazyLoad: \tab yes\cr
+#' }
+#'
+#' Test functionality of software as per unit tests.
+#' Compare functionality between versions of same software.
+#' Compare functionality between different software (given mapping of functionality).
+#' More information about each function can be found in 
+#' its help documentation.
+#'
+#' Some text
+#'
+#' The main functions are: 
+#' \code{\link{compileTestResults}}, \code{\link{compareTests}},  
+#' \code{\link{scanTestDir}}, \code{\link{findTestOutputs}},  
+#' 
+#' ...
+#' 
+#' @docType package
+#' @name Kanute-package
+NULL
